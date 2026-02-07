cdc9ab737cdf00a2569b9b7ddf6defbf1ed55d52
Ahmed-Elkemety <ahmedelkemety@gmail.com>
2026-02-07 17:38:53 +0200

    add-migraions to updatedatabase

---
+++ SAFQA.DAL/Migrations/20260207153559_remove user language.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SAFQA.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeuserlanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProfileCompleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsProfileCompleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Language",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
